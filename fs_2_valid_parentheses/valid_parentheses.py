cc666b26115415a38a191c1b63abba3e5412be04
povermanr <rachel.poverman@gmail.com>
2023-07-17 14:08:05 -0400

    21.2 data structures exercise

---
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -0,0 +1,37 @@
+def valid_parentheses(parens):
+    """Are the parentheses validly balanced?
+
+        >>> valid_parentheses("()")
+        True
+
+        >>> valid_parentheses("()()")
+        True
+
+        >>> valid_parentheses("(()())")
+        True
+
+        >>> valid_parentheses(")()")
+        False
+
+        >>> valid_parentheses("())")
+        False
+
+        >>> valid_parentheses("((())")
+        False
+
+        >>> valid_parentheses(")()(")
+        False
+    """
+
+    count = 0
+
+    for x in parens:
+        if x == '(':
+            count += 1
+        elif x == ')':
+            count -= 1
+
+        if count < 0:
+            return False
+
+    return count == 0
